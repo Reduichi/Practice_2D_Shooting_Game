024218adc119f1cec2d3d083fa7cd84c62bcc04a
Reduichi <57396815+Reduichi@users.noreply.github.com>
2019-12-05 21:35:14 +0800

    作業5-1未完成

--- UnityProject/Assets/Scripts/White.cs
+++ UnityProject/Assets/Scripts/White.cs
@@ -3,7 +3,31 @@
 public class White : MonoBehaviour
 {
     #region 練習區域 - 在此區域內練習
-    
+    [Header("子彈")]
+    public GameObject Bullet;
+    [Header("子彈發射位置")]
+    public Transform point;
+    [Header("音效區域")]
+    public AudioSource aud;
+    public AudioClip SoundShoot;
+
+    public void Shoot()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            aud.PlayOneShot(SoundShoot);
+            point.Translate(speed * Time.deltaTime, 0, 0);
+
+            // 三維向量 Vector3 - x,y,z
+            Vector3 pos = new Vector3(0, 0, 0);
+
+            // 四元數 Quaternion - x,y,z,w
+            Quaternion rot = new Quaternion(0, 0, 0, 0);
+            Instantiate(Bullet, pos, rot);
+        }
+    }
+
     #endregion
 
     #region KID 區域 - 不要偷看 @3@
